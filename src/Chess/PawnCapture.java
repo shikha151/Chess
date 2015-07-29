8e7fdb9329dcf2572996ba8b2612a1bd5a0240ab
rishabhkohli1994 <test@L-MAA-25004262.paypalcorp.com>
2015-07-29 15:40:36 +0530

    Pawn Capture Class Added

---
+++ src/Chess/PawnCapture.java
@@ -0,0 +1,31 @@
+package Chess;
+
+import java.util.ArrayList;
+
+public class PawnCapture implements Piece{
+
+	public static void main(String[] args) {
+
+		System.out.println(new PawnCapture().getPossibleInitialPositions("d3"));
+	}
+
+	@Override
+	public ArrayList<String> getPossibleInitialPositions(String finalPosition) {
+		ArrayList<String> possibleInitialPositions = new ArrayList<String>();
+		
+		int[][] moves = { { 1, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 } };
+		for(int [] i : moves)
+		{
+			if(positionExists((char)(finalPosition.charAt(0)+i[0])+""+(char)(finalPosition.charAt(1)+i[1])))
+				possibleInitialPositions.add((char)(finalPosition.charAt(0)+i[0])+""+(char)(finalPosition.charAt(1)+i[1]));
+		}
+		
+		return possibleInitialPositions;
+	}
+	private boolean positionExists(String possibleInitialPosition) {
+		return (('a' <= possibleInitialPosition.charAt(0) && 
+				possibleInitialPosition.charAt(0) <= 'h') && 
+				('1' <= possibleInitialPosition.charAt(1) && 
+				possibleInitialPosition.charAt(1) <= '8'));
+	}
+}
